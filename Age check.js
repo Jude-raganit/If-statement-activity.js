613b7a29f29ea54768b8e747738923540c3ab7e8
Jude-raganit <juderaganit56@gmail.com>
2024-10-10 16:12:22 +0800

    Create Age check.js

---
+++ Age check.js
@@ -0,0 +1,10 @@
+let age = 18
+      if ("age<=13"){
+         console.log("child");
+     }else if("age>=13 and age <=20"){
+        console.log ("teenager");
+     }else{
+        console.log("an adult");
+     }
+
+      
